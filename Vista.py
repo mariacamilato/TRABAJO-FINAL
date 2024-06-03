5e0c6fae976317afbc8a0d9916e5d947d79d71de
mariacamilato <mariac.tobon@udea.edu.co>
2024-06-03 17:13:58 -0500

    intento de gif

--- Vista.py
+++ Vista.py
@@ -1,7 +1,12 @@
-import sys 
-from PyQt5.QtWidgets import QApplication,QMainWindow, QDialog, QMessageBox,QLineEdit,QTextEdit,QWidget,QFileDialog 
+from PyQt5.QtWidgets import QApplication,QMainWindow, QDialog, QMessageBox,QLineEdit,QTextEdit,QWidget,QFileDialog,QLabel 
 from PyQt5.QtGui import QRegExpValidator, QIntValidator, QPixmap 
 from PyQt5.QtCore import Qt,QRegExp
+from PyQt5.uic import loadUi
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+from PySide6.QtGui import QMovie
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox, QLabel, QWidget
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import Qt
 from PyQt5.uic import loadUi
 
 class Login(QWidget):
@@ -88,6 +93,11 @@ class Plataforma(QMainWindow):
     
     def pageInicio(self):
         self.stackedWidget.setCurrentIndex(0)
+        self.label_animation = self.findChild(QLabel, 'label_animation')
+        if self.label_animation: 
+            self.movie = QMovie("imagenes\9v0u.gif")
+            self.label_animation.setMovie(self.movie)
+            self.movie.start()
 
     def pageExplora(self):##################################
         self.stackedWidget.setCurrentIndex(1)
